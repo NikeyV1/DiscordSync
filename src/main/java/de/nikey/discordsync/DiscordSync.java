34fa2f35ab2a185d4fba6dddecfac4bff3f49559
Nikey <130069847+NikeyV1@users.noreply.github.com>
2024-07-18 20:03:19 +0200

    Bug fix may be

--- src/main/java/de/nikey/discordsync/DiscordSync.java
+++ src/main/java/de/nikey/discordsync/DiscordSync.java
@@ -71,13 +71,7 @@ public final class DiscordSync extends JavaPlugin {
     public void onDisable() {
         String ip = getConfig().getString("server-ip");
         sendEmbed("Server is stopping!",ip,Color.RED);
-
-        try {
-            wait(500);
-            if (jda != null) jda.shutdownNow();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        if (jda != null) jda.shutdownNow();
     }
 
     public static void sendEmbed(String content, String ip, Color color) {
